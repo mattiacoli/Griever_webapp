fb0694a036e99a636128f5016016eff8ae7cdb52
mattiacoli <mattia.coli@aol.com>
2025-08-14 12:53:17 +0200

    add image and Swiper.js components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,21 @@ export default function Header() {
   return (
     <header>
       <nav className="navbar d-flex">
-        <Link to={"/"} className="navbar-brand ">
-          <img src="../assets/img/Logo_bw.png" alt="" />
-        </Link>
+        <NavLink className="navbar-brand" to="/">
+          <img src="/assets/img/Logo_bw.png" alt="logo" className="logo" style={{ width: "50px" }} />
+        </NavLink>
+
+        <div className="utils d-flex gap-1">
+          <Link to="/" className="utils_link" >
+            Home
+          </Link>
+          <Link to="/about" className="utils_link">
+            About
+          </Link>
+          <Link to="/contact" className="utils_link">
+            Contact
+          </Link>
+        </div>
       </nav>
     </header>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,39 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/bundle';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+
 export default function Homepage() {
   return (
-    <div className="homepage">
-      <h1>GRIEVER</h1>
-      <h3>LOVES SHY GUY</h3>
+    <div className="homepage ">
+      <div className="welcome text-center">
+        <h1>GRIEVER</h1>
+        <h3>LOVES SHY GUY</h3>
+        <p>Benvenuti nella pagina dei ragazzi timidi</p>
+      </div>
+
+      <div className="container">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          spaceBetween={50}
+          slidesPerView={1}
+          autoplay={{ delay: 2500, disableOnInteraction: true }}
+          loop={true}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide>
+            <img src="/assets/img/DSC_0139.JPG" alt="Slide 1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/assets/img/DSC_0142.JPG" alt="Slide 2" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/assets/img/DSC_0171.JPG" alt="Slide 3" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
