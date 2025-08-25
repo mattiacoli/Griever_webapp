fa81e0f59f22e5bfba5069fe32e7d4f919cf9644
mattiacoli <mattia.coli@aol.com>
2025-08-25 11:13:02 +0200

    Refactor Homepage layout and update styles for improved design consistency

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,16 +3,19 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function Homepage() {
   return (
-    <div className="homepage ">
+    <div className="homepage w-100">
       <div className="welcome text-center">
         <h1>GRIEVER</h1>
         <h3>LOVES SHY GUY</h3>
         <p>Benvenuti nella pagina dei ragazzi timidi</p>
       </div>
 
+      <section className="slider">
+        <Slider />
+      </section>
 
-      <Slider />
-      <section className="socials my-3">
+
+      <section className="socials my-2">
         <h2 className="text-center">FOLLOW US</h2>
         <div className="social-icons text-center">
           <a href="https://open.spotify.com/artist/2QXqvD5l5HpnpS95kcEvl3?si=g9vfIxaXTp2FfCNwLUhhaw" className="mx-3" style={{ fontSize: "2rem" }}>
